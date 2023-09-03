bda1b787fe9c2168079375598e9cb9a542784e34
Prerna kumari <122382178+Prernakumari312@users.noreply.github.com>
2023-09-04 00:18:53 +0530

    4

---
+++ check_positive_negative.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    if(n>0){
+        cout<<"postive number";
+
+    }
+    else if(n<0){
+        cout<<"negative number";
+    }
+    else{
+        cout<<"zero";
+    }
+}
