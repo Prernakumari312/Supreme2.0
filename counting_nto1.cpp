7a910d345dc348b3e1bc9c88ab87454f82d51570
Prerna kumari <122382178+Prernakumari312@users.noreply.github.com>
2023-09-04 00:23:42 +0530

    7

---
+++ counting_nto1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    int i=n;
+    while(i>=1){
+        cout<<i<<endl;
+        i--;
+    }
+
+    
+    
+}
