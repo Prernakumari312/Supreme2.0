60226dd52f076bba4031ed9e96385ea26da835e6
Prerna kumari <122382178+Prernakumari312@users.noreply.github.com>
2023-09-03 10:07:09 +0530

    2

---
+++ average.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,average;
+    cout<<"enter the value of a, b , c";
+    cin>>a>>b>>c;
+    average=(a+b+c)/3;
+    cout<<average;
+}
