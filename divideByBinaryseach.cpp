dad04c255d1b8cafb81f939f481eaca559f08002
Prerna kumari <122382178+Prernakumari312@users.noreply.github.com>
2023-11-07 08:36:49 +0530

    41

---
+++ divideByBinaryseach.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int getQuotient(int divisor,int dividend){
+    int s=0;
+    int e=dividend;
+    int ans=-1;
+    int mid=s+(e-s)/2;
+
+    while(s<=e){
+        if(mid*divisor==dividend){
+            return mid;
+
+        }
+        if(mid*divisor<dividend){
+            ans=mid;
+            s=mid+1;
+
+        }
+        if(mid*divisor>dividend){
+            e=mid-1;
+        }
+
+         mid=s+(e-s)/2;
+
+    }
+
+    return ans;
+
+}
+
+int main(){
+    int divisor=7;
+    int dividend=29;
+
+    int ans=getQuotient(divisor,dividend);
+    cout<<"ans is :"<<ans;
+}
