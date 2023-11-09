6505eabcd198007ba29095299f3a00baf62dd152
Prerna kumari <122382178+Prernakumari312@users.noreply.github.com>
2023-11-09 09:57:10 +0530

    43

---
+++ length_of_string.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int findlength(char ch[],int size){
+    int length=0;
+
+    for(int i=0;i<size;i++){
+        if(ch[i]=='\0'){
+            break;
+        }
+        else{
+            length++;
+        }
+    }
+    return length;
+}
+
+int main(){
+    char ch[100];
+    cin>>ch;
+    int len=findlength(ch,100);
+    cout<<"Length of string is:"<<len<<endl;
+
+}
